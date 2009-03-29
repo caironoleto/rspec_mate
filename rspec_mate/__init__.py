f7c9167bf89625a402123350e8e9e017e97268ae
Cairo Noleto <caironoleto@gmail.com>
2009-03-29 11:05:26 -0300

    removing files without use and fix all bugs

--- rspec_mate/__init__.py
+++ rspec_mate/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 - Nando Vieira
+# Copyright (C) 2009 - Cairo Noleto and Cleiton Francisco
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,10 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #
-# 2007-10-25 - Alexandre da Silva <simpsomboy@gmail.com>
-# Obtained original program from Nando Vieira, and changed need use only
-# python and mozembed, removed not used code
-
+# Obtained original program from Nando Vieira and Alexandre da Silva
 
 from gettext import gettext as _
 import gedit
@@ -43,24 +40,15 @@ ui_str = """
 </ui>
 """
 
-DEBUG_NAME = 'RM_DEBUG'
-DEBUG_TITLE = 'RSpec_Mate'
-
 class BrowserPage(webkit.WebView):
     def __init__(self):
         webkit.WebView.__init__(self)
 
-def debug(text, level=1):
-    if os.environ.has_key(DEBUG_NAME):
-        try:
-            required_level = int(os.environ[DEBUG_NAME])
-
-            if required_level >= level:
-                print "[%s] %s" % (DEBUG_TITLE, text)
-        except:
-            print "[%s] debug error" % DEBUG_TITLE
 # Link Pattern (in line)
 lp = re.compile('([^/]*)(((\/[a-zA-Z0-9-_\.]+)+):(\d+):)(.*)$')
+
+#Title Pattern
+tp = re.compile('.*1">([a-zA-Z0-9-_\. ]*)</dt>.*')
 
 #Root path pattern
 rp = re.compile('(.*)\/spec\/.*')
@@ -81,32 +69,34 @@ def get_root_path(uri):
     if result:
       uri = result.group(1)
     return uri
+
+def get_title(title):
+    result = tp.match(title)
+    if result:
+      title = result.group(1)
+      return title
+    return None
     
 def file_link(file, line=0):
     return "gedit:///%s?line=%d" % (file,line)
 
-# TODO: Create a Configuragion dialog
 class RspecPlugin(gedit.Plugin):
     def __init__(self):
         gedit.Plugin.__init__(self)
         self.instances = {}
 
     def activate(self, window):
-        debug('activating plugin')
         self.instances[window] = RspecWindowHelper(self, window)
 
     def deactivate(self, window):
-        debug('deactivating plugin')
         self.instances[window].deactivate()
         del self.instances[window]
 
     def update_ui(self, window):
-        debug('updating ui')
         self.instances[window].update_ui()
 
 class RspecWindowHelper:
     handlers = {}
-
     mt = re.compile(r'(?P<protocol>^gedit:\/\/)(?P<file>.*?)\?line=(?P<line>.*?)$')
 
     def __init__(self, plugin, window):
@@ -118,8 +108,6 @@ class RspecWindowHelper:
         self.add_menu()
 
     def deactivate(self):
-        debug('deactivate function called')
-
         self._browser = None
         self.rspec_window = None
         self.window = None
@@ -130,31 +118,12 @@ class RspecWindowHelper:
             ('RSpecTwo', gtk.STOCK_EDIT, _('Run all specs'), '<Super>r', _("Press Super + r to run all specs"), self.run_all_specs),
             ('RSpecOne', gtk.STOCK_EDIT, _('Run current spec'), '<Super>s', _("Press Super + s to run current spec"), self.run_current_spec)
         ]
-
         action_group = gtk.ActionGroup("RSpecActions")
         action_group.add_actions(actions, self.window)
 
         self.manager = self.window.get_ui_manager()
         self.manager.insert_action_group(action_group, -1)
         self.manager.add_ui_from_string(ui_str)
-
-    def get_root_directory(self):
-        # get filebrowser plugin root
-        fb_root = self.get_filebrowser_root()
-        # get eddt plugin root
-        eddt_root = self.get_eddt_root()
-
-        if fb_root and fb_root != "" and fb_root is not None:
-            title = "RSpec (Filebrowser integration)"
-            root = fb_root
-        elif eddt_root and eddt_root != "" and eddt_root is not None:
-            title = "RSpec (EDDT integration)"
-            root = eddt_root
-        else:
-            title = "RSpec (current directory)"
-            root = os.path.dirname(__file__)
-
-        return (root.replace("file://", ""), title)
 
     # taken from snapopen plugin
     def get_filebrowser_root(self):
@@ -173,23 +142,9 @@ class RspecWindowHelper:
 
         return val.get_string()
 
-    # taken from snapopen plugin
-    def get_eddt_root(self):
-        base = u'/apps/gedit-2/plugins/eddt'
-        client = gconf.client_get_default()
-        client.add_dir(base, gconf.CLIENT_PRELOAD_NONE)
-        path = os.path.join(base, u'repository')
-        val = client.get(path)
-
-        if val is not None:
-            return val.get_string()
-
     def run_all_specs(self, *args):
-        root, title = self.get_root_directory()
-        doc = self.window.get_active_document()
-        str_uri = doc.get_uri()
-        uri = gnomevfs.URI(str_uri)
-        spec_path = get_root_path(uri.path) + "/spec/"
+        uri = get_file_path(self.get_filebrowser_root())
+        spec_path = get_root_path(uri) + "/spec/"
         os.system("spec %s -f h:%s" % (spec_path, TMP_FILE))
 
         if self.rspec_window:
@@ -207,18 +162,17 @@ class RspecWindowHelper:
             self.rspec_window.add(self._browser)
             self.rspec_window.show_all()
 
-        self.rspec_window.set_title(title)
         f = open(TMP_FILE)
         html_str = ''
         for l in f.readlines():
             html_str += get_line(l)
+        
+        self.rspec_window.set_title("All Specs")
         self._browser.load_string(html_str, "text/html", "utf-8", "about:")
 
-        # remove the temporary file after load to avoid any security issue
         os.unlink(TMP_FILE)
 
     def run_current_spec(self, *args):
-        root, title = self.get_root_directory()
         doc = self.window.get_active_document()
         str_uri = doc.get_uri()
         uri = gnomevfs.URI(str_uri)
@@ -239,14 +193,19 @@ class RspecWindowHelper:
             self.rspec_window.add(self._browser)
             self.rspec_window.show_all()
 
-        self.rspec_window.set_title(title)
         f = open(TMP_FILE)
         html_str = ''
+        title = "Current Spec"
         for l in f.readlines():
             html_str += get_line(l)
+            _title = get_title(l)
+            if _title is not None:
+              title = _title
+
+        print title
+        self.rspec_window.set_title("Spec to %s" % (title))
         self._browser.load_string(html_str, "text/html", "utf-8", "about:")
 
-        # remove the temporary file after load to avoid any security issue
         os.unlink(TMP_FILE)
 
     def on_rspec_close(self, *args):
